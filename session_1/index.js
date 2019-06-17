cd56fcc4aa0f2ff5d621ead5f02e19bb96e43b4a
anhthuanprobg <lanh3883@gmail.com>
2019-06-17 22:06:49 +0700

    first files

---
+++ session_1/index.js
@@ -0,0 +1,31 @@
+
+// // // const number = Number(prompt("x:"));
+// // // const numb = Number(prompt("y:"));
+// // // console.log(number+numb);
+
+// // // for(let i = 0; i < 100; i++){
+// // //     fd(100) 
+// // //     rt(90)
+// // //     fd(100)
+// // //     rt(90)
+// // //     fd(100)
+// // //     rt(90)
+// // //     fd(100)
+// // //     rt(90)
+// // //     rt(30)
+// // // }
+// // for(let y = 0; y <5; y+=1 ){
+// //     console.log(y);
+// //     console.log("ahihi");
+// //     // console.log(index);
+// }
+for(let y= 0; y < 101;y+=1 ){
+    console.log(y)
+    
+}
+for (let y = 1; y < 101; y+=5) {
+    console.log(y)
+}
+for(let y = 100; y > 0 ; y-=3 ){
+    console.log(y);
+}
