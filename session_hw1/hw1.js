d4def6ad8ef4ce7537f5d5cc5904aec3686336ee
anhthuanprobg <lanh3883@gmail.com>
2019-06-19 23:54:24 +0700

    a

---
+++ session_hw1/hw1.js
@@ -0,0 +1,57 @@
+// // // // // // // 1.
+// // // // // // // To check a variable's type we use typeof
+// // // // // // 
+// // // // // // -Missing semicolons at the end of a line.
+// // // // // // -Curly braces without an if, for, while, etc.
+// // // // // // -Code that is never run because of a return, throw, continue, or break.
+// // // // // // -Case statements in a switch that do not have a break statement.
+// // // // // // -Leading and trailing decimal points on a number.
+// // // // // // -A leading zero that turns a number into octal (base 8).
+// // // // // // -Comments within comments.
+// // // // // // -Ambiguity whether two adjacent lines are part of the same statement.
+// // // // // // -Statements that don't do anything.
+// // // // // // 2
+// // // // // let radius = Number(prompt("Radius:"));
+// // // // // let area = (3.14*radius*radius);
+// // // // // // console.log("Area",area)
+
+// // // // // 3
+// // // // let temp = Number(prompt("Enter the temperature in Celcius"))
+// // // // let f = ((temp*1.8)+32);
+// // // // console.log("Fahrenheit:",f)
+
+
+// // // // #square
+// // // clear()
+// // // fd(100)
+// // // rt(90)
+// // // fd(100)
+// // // rt(90)
+// // // fd(100)
+// // // rt(90)
+// // // fd(100)
+// // // rt(90)
+
+
+// // // #triangle
+// // clear()
+// // rt(30)
+// // fd(200)
+// // rt(120)
+// // fd(200)
+// // rt(120)
+// // fd(200)
+
+// // #Circle
+// clear();
+//  for(let i=1; i<360; i++) {
+//    fd(2)
+//    rt(1)
+//  }
+
+
+// #multi circle
+clear()
+for(let i=1, i<360; i++) {
+    
+}
