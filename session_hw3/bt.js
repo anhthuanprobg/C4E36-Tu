977f902280824bfc20c0235420b1682d18806ba6
anhthuanprobg <lanh3883@gmail.com>
2019-07-01 18:45:23 +0700

    a

---
+++ session_hw3/bt.js
@@ -0,0 +1,46 @@
+// A nested list is a list that appears as an element in another list
+
+//nested = [36,"abc","123"] 
+
+
+
+// const menu = ["T-Shirt", "Sweater" ]
+// console.log(menu)
+// const answer = prompt("Welcome to our shop, what do you want (C, R, U, D)?").toLowerCase();
+// if(answer ==="r"){
+//     console.log(menu)
+// } else if(answer ==="c"){
+//     let newClothes= prompt("Enter new items:")
+//     console.log(`Our items: ${itemsList}`)
+// } else if(answer === "u"){
+//     let pos = prompt("Update position")
+//     let new_item = prompt("New item?")
+//     menu[pos - 1] = new_item
+//     console.log(`Our items: ${menu}`)
+// } else if(answer ==="d"){
+//     let del = prompt("Delete position ?")
+//     menu.splice(del - 1, 1);
+//     console.log(`Our items: ${menu}`);
+// }
+
+
+
+// let flock = [5, 7, 300, 90, 24, 50, 75]
+// console.log(`Hello, my name is tu and these are my sheeps sizes ${flock.join(', ')}`);
+
+// function sold() {
+//     let max = flock[0];
+// for(i = 0; i < flock.length; i++){
+//     if (max < flock[i]){
+//         max = flock[i];
+//     }
+// } console.log(`Now my biggest sheep has size ${max} and let's shear it`);
+// }
+
+
+//  list[list.indexOf(max)] = 8;
+
+//  console.log(`After shearing, here is my flock: ${flock.join(', ')}`);
+
+
+
